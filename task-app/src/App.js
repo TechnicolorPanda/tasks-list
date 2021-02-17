462cbadc9a08a04246967c740741d546134c8e88
LorriAnn <LorriAnn7@att.net>
2021-02-16 20:51:55 -0500

    converts class to hooks

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,61 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Overview from "./components/Overview";
 import './App.css';
 import uniqid from 'uniqid';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
 
-    this.state = {
-      taskArray: [],
-      task: '',
-      submit: false,
-    };
-  }
+  const [taskArray, setTaskArray] = useState([]);
+  const [task, setTask] = useState('');
+  const [submit, setSubmit] = useState(false);
 
-  handleChange = (event) => {
-    console.log(event.target.value);
-    this.setState({
-      task: event.target.value,
-    });
+  const handleChange = (event) => {
+    event.preventDefault();  
+    setTask(event.target.value);
+    setSubmit(false);
   };
 
-  onSubmitTask = (event) => {
+  const onSubmitTask = (event) => {
     event.preventDefault();
-
-    this.setState({
-      taskArray: [
-        {task: this.state.task},
-      ]
-    });
-
-    this.setState({
-      taskArray: this.state.taskArray.concat(this.state.task),
-      task: '',
-      submit: true,
-    });
-    console.log(this.state.taskArray);
+    // setTask(event.target.value);
+    setTaskArray(taskArray => taskArray.concat(task));
+    setTask('');
+    setSubmit(true);
   };
 
   //TODO: correct index number to delete right item
 
-  deleteTask = (index) => {
+  const deleteTask = (index) => {
     console.log(index);
-    const taskArray = Object.assign([], this.state.taskArray);
-    taskArray.splice(index, 1);
-    this.setState({taskArray: taskArray});
+    // setTaskArray(taskArray => Object.assign([], taskArray));
+    // taskArray.splice(index, 1);
   }
-
-  render() {
-    const { task, taskArray, submit } = this.state;
 
     return (
       <div>
-        <form onSubmit={this.onSubmitTask}>
+        <form onSubmit={onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
           <input
-            onChange={this.handleChange}
+            onChange={handleChange}
             value={task}
             type="text"
             id="taskInput"
@@ -63,20 +44,18 @@ class App extends Component {
           <button type="submit">Add Task</button>
         </form>
 
-      { submit 
-        ? taskArray.map((task, index) => {
-        return(<div key = {uniqid()}><Overview 
-          deleteEvent={this.deleteTask.bind(this)}
-          task = {task} 
-          number = {index}
-          id = {uniqid()}
-        /></div>)
-        })
+        {submit 
+          ? taskArray.map((task, index) => {
+            return(<Overview key = {uniqid()}
+              deleteEvent={deleteTask.bind(this)}
+              task = {task} 
+              number = {index}
+          />
+          )})
         : null
       }
-          </div>
+      </div>
     );
-  };
 }
 
 export default App;
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -4,20 +4,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 const Overview = (props) => {
-  const listNumber = props.number + 1;
-  const task = props.task;
+  const { 
+    task,
+    number,
+  } = props;
+
+  console.log(task);
+
   const trashIcon = <FontAwesomeIcon icon = {faTrash} />
   const editIcon = <FontAwesomeIcon icon = {faEdit} />
-  console.log(props.id);
 
   return (
     <ul>
-      <li>  {listNumber} | {task} 
-      <button onClick = {props.deleteEvent}>{trashIcon}</button>
-      <button>{editIcon}</button>
+      <li>
+        {' '}
+        {number + 1}
+        {' '}
+        |
+        {' '}
+        {task}
+        <button onClick={props.deleteEvent}>{trashIcon}</button>
+        <button>{editIcon}</button>
       </li>
     </ul>
-    );
-};
+  );
+}
 
 export default Overview;
