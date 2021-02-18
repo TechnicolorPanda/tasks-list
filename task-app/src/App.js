b50e17782aa41d4ad00316ed0a46448801543e47
LorriAnn <LorriAnn7@att.net>
2021-02-17 20:34:46 -0500

    debugs delete function

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -5,9 +5,13 @@ import uniqid from 'uniqid';
 
 const App = () => {
 
+  // declares states
+
   const [taskArray, setTaskArray] = useState([]);
   const [task, setTask] = useState('');
   const [submit, setSubmit] = useState(false);
+
+  // handles input change upon input
 
   const handleChange = (event) => {
     event.preventDefault();  
@@ -15,47 +19,47 @@ const App = () => {
     setSubmit(false);
   };
 
+  // creates array to pass to Overview component
+
   const onSubmitTask = (event) => {
     event.preventDefault();
-    // setTask(event.target.value);
+    setTask(event.target.value);
     setTaskArray(taskArray => taskArray.concat(task));
     setTask('');
     setSubmit(true);
   };
 
-  //TODO: correct index number to delete right item
+  // deletes task up selection
 
   const deleteTask = (index) => {
-    console.log(index);
-    // setTaskArray(taskArray => Object.assign([], taskArray));
-    // taskArray.splice(index, 1);
+    setTaskArray(taskArray => taskArray.filter((taskArray, i) => i !== index));
   }
 
-    return (
-      <div>
-        <form onSubmit={onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input
-            onChange={handleChange}
-            value={task}
-            type="text"
-            id="taskInput"
-          />
-          <button type="submit">Add Task</button>
-        </form>
+  return (
+    <div>
+      <form onSubmit={onSubmitTask}>
+        <label htmlFor="taskInput">Enter task</label>
+        <input
+          onChange={handleChange}
+          value={task}
+          type="text"
+          id="taskInput"
+        />
+        <button type="submit">Add Task</button>
+      </form>
 
-        {submit 
-          ? taskArray.map((task, index) => {
-            return(<Overview key = {uniqid()}
-              deleteEvent={deleteTask.bind(this)}
-              task = {task} 
-              number = {index}
+      {submit 
+        ? taskArray.map((task, index) => {
+          return(<Overview key = {uniqid()}
+            deleteEvent={deleteTask.bind(task, index)}
+            task = {task} 
+            number = {index}
           />
-          )})
+        )})
         : null
       }
-      </div>
-    );
+    </div>
+  );
 }
 
 export default App;
